e66b02b2870a893b766308ed09aca0267b8d73fe
efarioli <efarioli@gmail.com>
2022-06-11 14:42:23 +0100

    first commit

---
+++ src/min_window_substring.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author f023507i
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. Jun 2022 13:52
+%%%-------------------------------------------------------------------
+-module(min_window_substring).
+-author("Zeke Farioli").
+
+%% API
+-export([min_window/2]).
+
+min_window(S, T) ->
+  ok.
