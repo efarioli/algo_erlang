70217f35d8404c95c71aaa49d8897a941ef9f9ca
efarioli <efarioli@gmail.com>
2022-06-11 15:29:27 +0100

    get map with all keys equals to zero

--- src/min_window_substring.erl
+++ src/min_window_substring.erl
@@ -13,15 +13,17 @@
 -export([min_window/2]).
 
 min_window(S, T) ->
-  TWindow = map_count(T),
+  {TWindow, Window} = map_count(T),
   ok.
 
 map_count(Str) ->
   Dic = dict:new(),
-  map_count(Str, Dic).
+  map_count(Str, Dic, dict:new()).
 
-map_count([], Dic) ->
-  dict:to_list(Dic);
-map_count([H|T], Dic) ->
+map_count([], Dic, DicZeros) ->
+  {Dic, DicZeros};
+map_count([H|T], Dic, DicZeros) ->
   Dic2 = dict:update([H], fun([X]) -> [X + 1] end, [1], Dic),
-  map_count(T,Dic2).
+  Dic3 = dict:update([H], fun([_]) -> [0] end, [0], DicZeros),
+  map_count(T,Dic2,Dic3).
+
