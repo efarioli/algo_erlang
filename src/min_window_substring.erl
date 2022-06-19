84febc44a90550e13f9351027a9483fbe8c9050a
efarioli <efarioli@gmail.com>
2022-06-19 21:47:18 +0100

    algorithm working and passing all tests

--- src/min_window_substring.erl
+++ src/min_window_substring.erl
@@ -12,86 +12,181 @@
 %% API
 -export([min_window/2]).
 
-min_window(S, T) ->
-  {TWindow, Window} = map_count(T),
-
-  Res = min_window(S, Window, TWindow, 0 , dict:size(TWindow), [], {999999,0,0}, 0, 0),
-  io:format("~p~n", [Res]),
-  ok.
-
-map_count(Str) ->
-  Dic = dict:new(),
-  map_count(Str, Dic, dict:new()).
-
-map_count([], Dic, DicZeros) ->
-  {Dic, DicZeros};
-map_count([H|T], Dic, DicZeros) ->
-  Dic2 = dict:update([H], fun([X]) -> [X + 1] end, [1], Dic),
-  Dic3 = dict:update([H], fun([_]) -> [0] end, [0], DicZeros),
-  map_count(T,Dic2,Dic3).
+min_window(S,T) ->
+  S1 = binary_to_list(S),
+  T1 = binary_to_list(T),
+  min_window_sub(S1, T1).
 
 
-min_window([], Window, TWindow, Have, Needed, Acc, {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx, EndIdx) ->
-  {Acc, MinDiff, BegIdxFinal, EndIdxFinal};
 
-min_window([H|T], Window, TWindow, Have, Have , Acc, {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx, EndIdx) ->
-  timer:sleep(1000),
-  io:format("--------------------------~n~p~n~p~n~p~n~p~n~p~n~p~n", [[H] , {T, dict:to_list(Window), dict:to_list(TWindow)}, {Have, Have}, {Acc}, {MinDiff, BegIdxFinal, EndIdxFinal}, { BegIdx, EndIdx}]),
+min_window_sub(S,S) ->
+  list_to_binary( S);
 
-
-  Last = lists:last(Acc),
-
-  Acc2 = lists:droplast(Acc),
-  io:format("~p~n",[Acc2]),
-  io:format("---~p~n",[[Last]]),
-  MinDiff2 = EndIdx - BegIdx,
-  {MinDiff3, BegIdxFinal3, EndIdxFinal3} = case MinDiff2 < MinDiff of
-                                   true ->
-                                     {MinDiff2, BegIdx, EndIdx};
-                                   false ->
-                                     {MinDiff, BegIdxFinal, EndIdxFinal}
-
-                                 end,
-  io:format("~p~n",[{MinDiff3, BegIdxFinal3, EndIdxFinal3}]),
-  io:format("---------------------------------------~n"),
-  case dict:is_key([Last], TWindow) of
-
-    true ->
-      io:format("~p~n",[{iskey_true, [Last]}]),
-      Window2 = dict:update([Last], fun([X]) -> [X + 1] end, [1], Window),
-      [WValue] = dict:fetch([Last],Window2),
-      [TWValue] = dict:fetch([Last],TWindow),
-      Have2 = case WValue =< TWValue of
-                true ->
-                  Have - 1;
-                false ->
-                  Have
-              end,
-      min_window([H|T], Window, TWindow, Have2, Have,Acc2,{MinDiff3, BegIdxFinal3, EndIdxFinal3}, BegIdx+1, EndIdx);
+min_window_sub(S,[One]) ->
+  case lists:member(One, S) of
+    true->
+      list_to_binary([One]);
     false ->
-      min_window(T, Window, TWindow, Have, Have, [H|Acc2], {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx+1, EndIdx)
-
+      <<"">>
   end;
 
-min_window([H|T], Window, TWindow, Have, Needed, Acc, {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx, EndIdx) ->
-  io:format("--------------------------~n~p~n~p~n~p~n~p~n~p~n~p~n", [[H] , {T, dict:to_list(Window), dict:to_list(TWindow)}, {Have, Needed}, {Acc}, {MinDiff, BegIdxFinal, EndIdxFinal}, { BegIdx, EndIdx}]),
-%%  io:format("~p~n", [{[H] , "\n",  T, dict:to_list(Window), dict:to_list(TWindow), "\n", Have, Needed,"\n", Acc, "\n", {MinDiff, BegIdxFinal, EndIdxFinal}, "\n", BegIdx, EndIdx}]),
-  case dict:is_key([H], TWindow) of
-    true ->
-      Window2 = dict:update([H], fun([X]) -> [X + 1] end, [1], Window),
-      [WValue] = dict:fetch([H],Window2),
-      [TWValue] = dict:fetch([H],TWindow),
-      Have2 = case WValue of
-                TWValue ->
-                  Have +1;
-                _ ->
-                  Have
-              end,
-      min_window(T, Window, TWindow, Have2, Needed,[H|Acc], {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx, EndIdx+1);
-    false ->
-      min_window(T, Window, TWindow, Have, Needed, [H|Acc], {MinDiff, BegIdxFinal, EndIdxFinal}, BegIdx, EndIdx+1)
-
+min_window_sub(S, T) ->
+  SLength = length(S),
+  TLength = length(T),
+  case TLength of
+    SLength ->
+      S1 =lists:sort(S),
+      T1 = lists:sort(T),
+      case S1 of
+        T1 ->
+          list_to_binary(S);
+        _ ->
+          <<"">>
+      end;
+    _ ->
+      case TLength > SLength of
+        true ->
+          <<"">>;
+        false ->
+          list_to_binary(min_window_sub2(S, T))
+      end
   end.
 
 
 
+min_window_sub2(S, T) ->
+  {TWindow, Window} = string_to_dict_count(T),
+  Array = to_map_like_array(S), % index key - value letter
+  Size = dict:size(Window),
+
+  {_MinDiff, Right,Left} = min_window_sub2(S,Window,TWindow, 0, 0, {999999, 0, 0},  0, Size,S, Array),
+  sub_s(S, Left+1, Right).
+
+
+min_window_sub2([],Window,TWindow, Left,Right,  {MinDiff, BegIdx, EndIdx}, Have, Have,OrigStr, Arr)->
+  NewDiff = Right - Left,
+  {MinDiff2, BegIdx2, EndIdx2} = case NewDiff < MinDiff of
+                                   true ->
+                                     {NewDiff, Right, Left};
+                                   false ->
+                                     {MinDiff, BegIdx, EndIdx}
+                                 end,
+  Letter = [get_from_map_type_array(Left, Arr)],
+  case dict:is_key(Letter,TWindow) of
+    true ->
+      NewWindow = dict:update(Letter, fun([X]) -> [X + -1] end,[0] , Window),
+      NWFetch = dict:fetch(Letter, NewWindow),
+      OldFetch = dict:fetch(Letter, TWindow),
+      NewHave = case NWFetch < OldFetch of
+                  true ->
+                    Have - 1;
+                  false ->
+                    Have
+                end,
+      min_window_sub2([],NewWindow,TWindow, Left+1,Right,  {MinDiff2, BegIdx2, EndIdx2}, NewHave, Have,OrigStr,Arr);
+    false ->
+      min_window_sub2([],Window,TWindow, Left+1,Right,  {MinDiff2, BegIdx2, EndIdx2}, Have, Have,OrigStr,Arr)
+
+  end;
+
+min_window_sub2([],_Window,_TWindow, Left,Right,  {MinDiff, BegIdx, EndIdx}, Have, Needed,_OrigStr,  _Arr)  ->
+  case Have >= Needed of
+    true ->
+      NewDiff = Right - Left,
+      {MinDiff2, BegIdx2, EndIdx2} = case NewDiff < MinDiff of
+                                       true ->
+                                         {NewDiff, Right, Left};
+                                       false ->
+                                         {MinDiff, BegIdx, EndIdx}
+                                     end,
+      {MinDiff2, BegIdx2, EndIdx2};
+    false ->
+      {MinDiff, BegIdx, EndIdx}
+
+  end;
+
+
+min_window_sub2([H|T],Window,TWindow, Left,Right,  {MinDiff, BegIdx, EndIdx}, Have, Have, OrigStr, Arr) ->
+  NewDiff = Right - Left,
+  {MinDiff2, BegIdx2, EndIdx2} = case NewDiff < MinDiff of
+                                   true ->
+                                     {NewDiff, Right, Left};
+                                   false ->
+                                     {MinDiff, BegIdx, EndIdx}
+                                 end,
+  Letter = [get_from_map_type_array(Left, Arr)],
+  case dict:is_key(Letter,TWindow) of
+    true ->
+      NewWindow = dict:update(Letter, fun([X]) -> [X + -1] end,[0] , Window),
+      NWFetch = dict:fetch(Letter, NewWindow),
+      OldFetch = dict:fetch(Letter, TWindow),
+      NewHave = case NWFetch < OldFetch of
+                  true ->
+                    Have - 1;
+                  false ->
+                    Have
+                end,
+      min_window_sub2([H|T],NewWindow,TWindow, Left+1,Right,  {MinDiff2, BegIdx2, EndIdx2}, NewHave, Have,OrigStr, Arr);
+    false ->
+      min_window_sub2([H|T],Window,TWindow, Left+1,Right,  {MinDiff2, BegIdx2, EndIdx2}, Have, Have,OrigStr, Arr)
+
+  end;
+
+min_window_sub2([H|T],Window,TWindow, Left,Right,  {MinDiff, BegIdx, EndIdx}, Have, Needed,OrigStr, Arr) ->
+  case dict:is_key([H],TWindow) of
+    true ->
+      NewWindow = dict:update([H], fun([X]) -> [X + 1] end,[0] , Window),
+      NWFetch = dict:fetch([H], NewWindow),
+      OldFetch = dict:fetch([H], TWindow),
+      NewHave = case NWFetch of
+                  OldFetch ->
+                    Have+1;
+                  _ ->
+                    Have
+                end,
+      min_window_sub2(T,NewWindow,TWindow, Left,Right+1,  {MinDiff, BegIdx, EndIdx}, NewHave, Needed,OrigStr, Arr);
+    false ->
+      min_window_sub2(T,Window,TWindow, Left,Right+1,  {MinDiff, BegIdx, EndIdx}, Have, Needed,OrigStr, Arr)
+
+
+  end.
+
+string_to_dict_count(Str) ->
+  Dict = dict:new(),
+  Dict2 = dict:new(),
+  string_to_dict_count(Str, Dict, Dict2).
+
+string_to_dict_count([], Dict, Dict2) ->
+  {Dict, Dict2};
+string_to_dict_count([H|T], Dict, Dict2) ->
+  NewDict = dict:update([H], fun([X]) -> [X + 1] end, [1], Dict),
+  NewDict2 = dict:update([H], fun([_X]) -> [0] end, [0], Dict2),
+  string_to_dict_count(T,NewDict,NewDict2).
+
+sub_s(Str, B, E) ->
+  sub_s(Str,B,E,0,[]).
+
+sub_s([],_B,_E,_C,Acc) ->
+  lists:reverse(Acc);
+sub_s([_H|_T],_B,E,E,Acc) ->
+  lists:reverse(Acc);
+sub_s([H|T],B,E, C, Acc) ->
+  case (C+1)>=B of
+    true ->
+      sub_s(T,B, E, C+1,[H|Acc]);
+    false ->
+      sub_s(T,B, E, C+1,Acc)
+  end.
+
+to_map_like_array(Str) ->
+  Size = length(Str),
+  to_map_like_array(Str, maps:new(),0, Size).
+
+to_map_like_array([], Map, _C, _Size) ->
+  Map;
+to_map_like_array([H|T], Map, C, Size) ->
+  Map2 = maps:put(C,H, Map),
+  to_map_like_array(T, Map2,C+1, Size).
+
+get_from_map_type_array(K,Map) ->
+  maps:get(K, Map).
